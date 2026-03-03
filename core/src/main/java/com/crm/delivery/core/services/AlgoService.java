6a70b95deadddea32782d260b4fefbb61ce8026a
VeagleV <lanselot4267@gmail.com>
2026-03-03 11:55:29 +0300

    dev thingy

--- core/src/main/java/com/crm/delivery/core/services/AlgoService.java
+++ core/src/main/java/com/crm/delivery/core/services/AlgoService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "algorithm-Service", url = "http://91.193.239.198:81/api/algo")
+@FeignClient(name = "algorithm-Service", url = "http://localhost:8082/api/algo")
 public interface AlgoService {
     @PostMapping("/startAlgorithm")
     ResponseEntity<?> startAlgorithm(AlgoRequest algoRequest);
